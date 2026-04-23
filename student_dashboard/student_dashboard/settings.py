b50edbfba9d16b6e9fb65f421db683e2f797cc8c
Dhruv Lakhe <ddlakhe23-comp@bvucoep.edu.in>
2026-04-24 00:06:22 +0530

    Fix formatting in settings.py for static files

--- student_dashboard/student_dashboard/settings.py
+++ student_dashboard/student_dashboard/settings.py
@@ -127,7 +127,7 @@ SESSION_COOKIE_AGE = 86400  # 24 hours
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
-
+#
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'dashboard', 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
