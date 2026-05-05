ba8803ca2dc0d7aa0132c0a9a0d4d69fa8895329
dhv017 <104574089+dhv017@users.noreply.github.com>
2026-05-05 15:30:34 +0530

    fifth

--- student_dashboard/settings.py
+++ student_dashboard/settings.py
@@ -128,8 +128,7 @@ SESSION_COOKIE_AGE = 86400  # 24 hours
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'dashboard', 'static')]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
